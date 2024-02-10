ce09fa202cbfce5eb8dfca6d795827a50c2b6559
Mt-Lampert <perrin.blog@web.de>
2024-02-10 07:05:09 +0100

    w: soft redirection '/' -> '/contacts'; silent mode

--- src/main.go
+++ src/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Go Fiber is up and running!")
-	})
+	app.Get("/", GetContacts)
+	app.Get("/contacts", GetContacts)
 
 	app.Listen(":5000")
+}
+
+func GetContacts(c *fiber.Ctx) error {
+	searchTerm := c.Query("q")
+
+	if searchTerm == "" {
+		// => return all contacts as a list
+		return c.SendString("No search string was given.")
+	}
+
+	// => return only the found contacts as a list
+	return c.SendString(
+		fmt.Sprintf("We have a search string: '%s'", searchTerm))
 }
